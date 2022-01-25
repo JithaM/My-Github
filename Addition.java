4e42197374e575bc4e5a2732b6c5bd6435c32d67
Nair <jitha.nair@finastra.com>
2022-01-25 08:25:00 +0530

    adding first java file

---
+++ Addition.java
@@ -0,0 +1,14 @@
+class Addition
+{
+public static void sum()
+{
+	int a=10;
+	int b=4;
+	int c=a+b;
+	System.out.println("Sum is " +c);
+}
+public static void main(String args[])
+{
+Addition.sum();
+}
+}
